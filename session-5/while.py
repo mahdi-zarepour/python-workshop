41349046fbcd5702c36806401ed77db5566cd14d
Mahdi-zarepour <mahdizarepour15@gmail.com>
2024-12-04 10:32:15 +0330

    session 5

---
+++ session-5/while.py
@@ -0,0 +1,74 @@
+"""
+Iteration
+
+while loop
+----------
+Structure:
+    - while <expr>:
+        <statements>
+     else:
+        <statements>
+"""
+
+number = 10
+
+# while number > 0:
+#     print(number)
+#     number -= 1 # number = number - 1
+
+# False <expr>
+# while 2 > 4:
+#     print('how this possible?')
+
+# Find Even numbers
+# while number > 0:
+#     if number % 2 == 0:
+#         print(number)
+#     number -= 1
+
+# else
+# while number > 0:
+#     print(number)
+#     number -= 1
+# else:
+#     print('Finished')
+
+# print('Finished')
+
+
+# Find 3!
+# while number > 0:
+#     print(number)
+#     if number == 3:
+#         print('The number three was found!!!')
+#         break
+#     number -= 1
+
+# Ignore 3!
+# while number > 0:
+#     number -= 1
+#     if number == 3:
+#         print('The number three was found!!!')
+#         continue
+#     print(number)
+
+# One line while loop
+# while number > 0: print(number); number -= 1
+# while number > 0: number -= 1; if True: print('foo') # SyntaxError: invalid syntax
+
+
+# numbers_list = []
+
+# while number > 0:
+#     if number % 2 == 0:
+#         numbers_list.append(number)
+#     number -= 1
+
+# sum = 0
+# iter = 0
+# number = 10
+# while iter < len(numbers_list):
+#     sum += numbers_list[iter]
+#     iter += 1
+
+# print(sum)
