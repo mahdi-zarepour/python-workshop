ccd8bf7c338549dcc5e32f7a1847081b0ef35343
Mahdi-zarepour <mahdizarepour15@gmail.com>
2024-12-15 14:40:43 +0330

    start lesson-7

--- session-7/for_loop.py
+++ session-7/for_loop.py
@@ -1,28 +1,9 @@
 """
-Manipulate List
+for loop
 
-List methods apply in-place because lists are mutable objects, 
-i.e. the list itself is modified
+info ...
 ----------
-add items:
-    - append(object):
-        Adds an element at the end of the list
-    - insert(index, object):
-        Adds an element at the specified position
-    - extend(iterable):
-        Returns the number of elements with the specified value
-read items:
-    - index(value, start=0, stop=sys.maxsize):
-        Returns the index of the first element with the specified value
-    - reverse():
-        Reverses the order of the list
-    - sort(*, key, reverse):
-        Sorts the list
-delete items:
-    - pop():
-        Removes the element at the specified position or remove last item
-    - remove():
-        Removes the first item with the specified value
-    - clear():
-        Removes all the elements from the list
+X:
+    - x1:
+        somethings
 """
